1a07d62276db3c94e9309adc621fd560e5fb459b
thedouglasaraujo <thedouglasaraujo@gmail.com>
2025-01-07 19:49:01 -0300

    feat(migrations): Cria migration para items

---
+++ db/migrate/20250107224133_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[8.0]
+  def change
+    create_table :items do |t|
+      t.string :description
+      t.boolean :done
+      t.datetime :due_date
+      t.references :list, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
